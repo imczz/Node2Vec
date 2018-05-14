b72acc6205250f2a4f42a7929c6080b230d8be9e
imczz <790608522@qq.com>
2018-05-14 23:00:00 +0800

    :construction::bookmark:进过初步实验，skip-gram的hs与ns的e应该分开，否则不收敛，完成了CBOW算法（HS,NS），开始修改CBOW的bug

--- Node2Vector/src/czzWord2Vec/Word2Vec.java
+++ Node2Vector/src/czzWord2Vec/Word2Vec.java
@@ -294,12 +294,14 @@ public class Word2Vec<T> {
 			int c;
 			int randomWindow;									//[1, windowSize]的随机窗口大小，使词语之间关系更为密切
 			Random rand = new Random();
-			HWord<T> contextX;								//中心词的上下文
+			HWord<T> word, contextX;								//中心词的上下文
 			int wordIndex, contextIndex, thetaIndex;		//中心词,上下文在词典中的索引号;西塔索引号
 			T contextWord;									//上下文
 			int label;								//负采样的Lω(u) u∈ω∪Neg(ω)
-			IVector ehs, ens;					//hs与ns的e
+			IVector e, ehs, ens;				//hs与ns的e
+			IVector xsum;						//CBOW的上下文求和
 			float f, g;
+			int cw;				//CBOW上下文计数
 			for(localIteratorNumber = 1; localIteratorNumber <= this.iteratorNumber; localIteratorNumber++) { //迭代次数
 				for(int sentenceIndex = 0; sentenceIndex < this.passags.getSentenceCount(); sentenceIndex++) {
 					T[] sentence = this.passags.getSentence(sentenceIndex);
@@ -308,11 +310,82 @@ public class Word2Vec<T> {
 						wordIndex = this.vocabulary.getWordIndex(sentence[sentence_position]);
 						if(wordIndex < this.vocabulary.getStartPointer()) continue;				//当前词被过滤
 						wordIndex -= this.vocabulary.getStartPointer();		//因为有单词被过滤，所以model,theta等数组索引改变位置
-						//word = this.vocabulary.getWord(sentence[sentence_position]);
+						word = this.vocabulary.getWord(sentence[sentence_position]);
 						this.learnRate *= 0.9999f;							//减小学习率
 						if (this.learnRate < this.startLearnRate * 0.0001f) this.learnRate = this.startLearnRate * 0.0001f;
 						if(this.modelType == ModelType.CBOW) {
-							//TODO
+							cw = 0;
+							e = new CVector(this.dimensions);		//e=0
+							xsum = new CVector(this.dimensions);			//xw = 0;
+							for (int i = randomWindow; i < this.windowSize * 2 + 1 - randomWindow; i++) {			//遍历当前词的上下文
+								if (i != windowSize)				//取上下文中，除了当前词
+								{
+									c =	sentence_position -	this.windowSize + i;			//c={sp-w,sp-w+1, ..., sp-1, sp+1, ..., sp+w-1, sp+w}
+									if (c <	0) continue;
+									if (c >= sentence.length) continue;			//for (int i = c; i < this.windowSize * 2 + 1 - c; i++)
+									contextWord = sentence[c];
+									contextIndex = this.vocabulary.getWordIndex(contextWord);
+									contextX = this.vocabulary.getWord(contextWord);
+									if(contextIndex == -1) continue;			//for (int i = c; i < this.windowSize * 2 + 1 - c; i++)
+									contextIndex -= this.vocabulary.getStartPointer();		//因为有单词被过滤，所以model,theta等数组索引改变位置
+									if(contextIndex < 0) continue;			//当前词被过滤掉了
+									xsum.add(this._models[contextIndex]);
+									cw++;
+								}
+							}//for (int i = randomWindow; i < this.windowSize * 2 + 1 - randomWindow; i++)
+							if (cw > 0) {				//避免除数为0
+								xsum.multiply(1f / cw);				//X /= cw;
+								// HIERARCHICAL	SOFTMAX
+								if (this.trainMethod == TrainMethod.HS || this.trainMethod == TrainMethod.BOTH) {
+									for	(int l = 0;	l <	word.code.size(); l++) {		//从huffman路径分层SoftMax
+										thetaIndex = word.point.get(l);
+										// Propagate hidden -> output
+										f = xsum.multiply(this._huffmanTheta[thetaIndex]);
+										if (f < -this.expTable.getMaxX()) continue;
+										else if (f > this.expTable.getMaxX()) continue;
+										else f = expTable.getSigmoid(f);								//sigmoid函数
+										// 'g' is the gradient multiplied by the learning rate
+										g = (1 - word.code.get(l) - f) * this.learnRate;				//偏导数乘学习率
+										// Propagate errors output -> hidden
+										e.add(this._huffmanTheta[thetaIndex].new_Multi(g));			//e += g * theta
+										// Learn weights hidden -> output
+										this._huffmanTheta[thetaIndex].add(xsum.new_Multi(g));			//theta += g * xm
+									}
+								}
+								// NEGATIVE	SAMPLING
+								if (this.trainMethod == TrainMethod.NS || this.trainMethod == TrainMethod.BOTH) {			
+									for (int d = 0; d < this.negative + 1; d++) {
+										if (d == 0)	{
+											thetaIndex = wordIndex;
+											label =	1;
+										}
+										else {
+											thetaIndex = this.vocabulary.negSamplingWord();
+											if (thetaIndex == wordIndex) continue;
+											label =	0;
+										}
+										f = xsum.multiply(this._negTheta[thetaIndex]);
+										if (f > this.expTable.getMaxX()) g = (label - 1) * this.learnRate;
+										else if (f < -this.expTable.getMaxX()) g = (label - 0) * this.learnRate;
+										else g = (label - expTable.getSigmoid(f)) * this.learnRate;
+										e.add(this._negTheta[thetaIndex].new_Multi(g));			//e += g * theta
+										this._negTheta[thetaIndex].add(xsum.new_Multi(g));			//theta += g * xm
+									}
+								}
+								// hidden -> in
+								for (int i = randomWindow; i < this.windowSize * 2 + 1 - randomWindow; i++) {
+									c =	sentence_position -	this.windowSize + i;			//c={sp-w,sp-w+1, ..., sp-1, sp+1, ..., sp+w-1, sp+w}
+									if (c <	0) continue;
+									if (c >= sentence.length) continue;			//for (int i = c; i < this.windowSize * 2 + 1 - c; i++)
+									contextWord = sentence[c];
+									contextIndex = this.vocabulary.getWordIndex(contextWord);
+									contextX = this.vocabulary.getWord(contextWord);
+									if(contextIndex == -1) continue;			//for (int i = c; i < this.windowSize * 2 + 1 - c; i++)
+									contextIndex -= this.vocabulary.getStartPointer();		//因为有单词被过滤，所以model,theta等数组索引改变位置
+									if(contextIndex < 0) continue;			//当前词被过滤掉了
+									this._models[contextIndex].add(e);
+								}
+							}
 						}//if(this.modelType == ModelType.CBOW)
 						else if(this.modelType == ModelType.Skip_gram) {
 							for (int i = randomWindow; i < this.windowSize * 2 + 1 - randomWindow; i++) {			//遍历当前词的上下文
@@ -344,14 +417,13 @@ public class Word2Vec<T> {
 											// Propagate errors output ->	hidden
 											ehs.add(this._huffmanTheta[thetaIndex].new_Multi(g));				//e += g * theta
 											// Learn weights hidden -> output
-											this._huffmanTheta[thetaIndex].add(this._models[wordIndex].new_Multi(g));
+											this._huffmanTheta[thetaIndex].add(this._models[wordIndex].new_Multi(g));	//theta += g * x
 										}
 										this._models[wordIndex].add(ehs);				// Learn weights input -> hidden
 									}
-									// NEGATIVE	SAMPLING
+									// NEGATIVE SAMPLING
 									if (this.trainMethod == TrainMethod.NS || this.trainMethod == TrainMethod.BOTH) {
-										for (int d = 0; d < this.negative + 1; d++)
-										{
+										for (int d = 0; d < this.negative + 1; d++) {
 											if (d == 0)
 											{
 												thetaIndex = wordIndex;
