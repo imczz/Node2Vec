ca8a320a43b9be6a7867585a7986058c235f273e
imczz <790608522@qq.com>
2018-05-25 03:52:16 +0800

    :bug:修复最大特征值的特征向量不正确（未归一化）问题，特征向量正负问题

--- Node2Vector/src/czzMatrix/PowerMethod.java
+++ Node2Vector/src/czzMatrix/PowerMethod.java
@@ -12,31 +12,34 @@ public class PowerMethod {
 		this.mat = m;
 	}
 	
+	/**
+	 * 求方阵m的（绝对值）最大特征值与对应的特征向量*/
 	public Eigen maxEigen() {
 		Eigen ret = null;
 		if(this.mat.isSquare()) {
 			ret = new Eigen();
 			int row = mat.getRow();				//行数或者列数，也是特征向量的维度
-			Matrix eigVector = new Matrix(row, 1, 1);				//列向量
-			Matrix x0 = mat.multiply(eigVector), x1;
-			Matrix y;
+			Matrix eigVector = new Matrix(row, 1, 1);				//（列）向量.初始化
+			Matrix x0 = mat.multiply(eigVector);
+			Matrix x1;
+			Matrix y = PowerMethod.divideMax(x0);					//归一化;
 			while(true) {
-				y = PowerMethod.normalized(x0);					//归一化
 				x1 = mat.multiply(y);
-				if(PowerMethod.subFNorm(x1, x0, 2) < 1e-4) {
+				if(PowerMethod.subFNorm(x1, x0, 2) < 1e-5) {
 					break;
 				}
-				x0 = y;
+				x0 = x1;
+				y = PowerMethod.divideMax(x1);					//归一化
 			}
 			ret.eigenvalue = PowerMethod.maxComponent(x1);
-			ret.eigenvector = y;
+			ret.eigenvector = PowerMethod.normalized(y);
 		}
 		return ret;
 	}
 	
 	/**
-	 * 将一个列向量归一化*/
-	public static Matrix normalized(Matrix vec) {
+	 * 将一个列向量除绝对值最大的分量*/
+	public static Matrix divideMax(Matrix vec) {
 		Matrix ret = null;
 		if(vec.getRow() > 0 && vec.getColumn() == 1) {			//列向量
 			ret = new Matrix();
@@ -47,16 +50,30 @@ public class PowerMethod {
 		return ret;
 	}
 	
+	/**
+	 * 将一个列向量除绝对值最大的分量*/
+	public static Matrix normalized(Matrix vec) {
+		Matrix ret = null;
+		if(vec.getRow() > 0 && vec.getColumn() == 1) {			//列向量
+			ret = new Matrix();
+			ret.copy(vec);
+			ret.multiply(1 / PowerMethod.getLength(ret));
+		}
+		return ret;
+	}
+	
+	/**
+	 * 列向量中最大的分量*/
 	private static float maxComponent(Matrix vec) {
 		float ret = 0;
 		if(vec.getRow() > 0 && vec.getColumn() == 1) {			//列向量
 			ret = vec.get(0, 0);
 			float maxAbs = Math.abs(ret);				//绝对值最大分量
 			float nowComponent, nowAbs;
-			for(int i = 0; i < vec.getRow(); i++) {
+			for(int i = 1; i < vec.getRow(); i++) {
 				nowComponent = vec.get(i, 0);
 				nowAbs = Math.abs(nowComponent);
-				if(Math.abs(nowAbs) > maxAbs) {
+				if(nowAbs > maxAbs) {
 					maxAbs = nowAbs;
 					ret = nowComponent;
 				}
@@ -75,6 +92,19 @@ public class PowerMethod {
 				ret += Math.pow(Math.abs(vec1.get(i, 0) - vec2.get(i, 0)), f);
 			}
 			ret = (float) Math.pow(ret, 1 / f);
+		}
+		return ret;
+	}
+	
+	/**
+	 * 列向量的模、长度、2-范数*/
+	public static float getLength(Matrix vec) {
+		float ret = 0;
+		if(vec.getRow() > 0 && vec.getColumn() == 1) {	
+			for(int i = 0; i < vec.getRow(); i++) {
+				ret += Math.pow(vec.get(i, 0), 2);
+			}
+			ret = (float) Math.sqrt(ret);
 		}
 		return ret;
 	}
--- Node2Vector/src/czzMatrix/PowerMethodTest.java
+++ Node2Vector/src/czzMatrix/PowerMethodTest.java
@@ -26,9 +26,27 @@ public class PowerMethodTest {
 		assertEquals(9.7202, eig.eigenvalue, 1e-4);
 		int i;
 		float[] eigvec = {-0.3716f, -0.6924f, -0.6185f};
+		int symble = 1;
+		if(eigvec[0] * eig.eigenvector.get(0, 0) < 0) symble = -1;			//协调特征向量正负
 		for(i = 0; i < eig.eigenvector.getRow(); i++) {
-			assertEquals(eigvec[i], eig.eigenvector.get(i, 0), 1e-4);
+			assertEquals(eigvec[i] * symble, eig.eigenvector.get(i, 0), 1e-4);			//特征向量正负问题
 		}
 	}
 
+	@Test
+	public void testPowerMethod2() {
+		float[][] m = {{1, 4, 3}, {4, 5, 6}, {3, 6, 9}};
+		Matrix mat = new Matrix();
+		mat.load(m);
+		PowerMethod pm = new PowerMethod(mat);
+		Eigen eig = pm.maxEigen();
+		assertEquals(14.9730, eig.eigenvalue, 1e-4);
+		int i;
+		float[] eigvec = {0.3263f, 0.5800f, 0.7465f};
+		int symble = 1;
+		if(eigvec[0] * eig.eigenvector.get(0, 0) < 0) symble = -1;			//协调特征值正负
+		for(i = 0; i < eig.eigenvector.getRow(); i++) {
+			assertEquals(eigvec[i] * symble, eig.eigenvector.get(i, 0), 1e-4);			//特征向量正负问题
+		}
+	}
 }
