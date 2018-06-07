2a12f6736a8403cc92812984560c2135029b95b6
imczz <790608522@qq.com>
2018-06-07 13:45:50 +0800

    :white_check_mark:经过测试轮盘赌法效率更高

--- Node2Vector/src/czzSelectItem/SelectorTest.java
+++ Node2Vector/src/czzSelectItem/SelectorTest.java
@@ -11,10 +11,45 @@ import org.junit.Test;
  @author CZZ*/
 public class SelectorTest {
 	int repeatNum = 10000000;				//测试过100000000，约7.91s
+	int repeatNum2 = 100000000;				//测试过100000000，约7.91s
 	float delta = 1e-3f;
+	
+	final int expectN = 30;
+	float[] expect = new float[expectN];
+	
 	
 	@Before
 	public void setUp() throws Exception {
+		expect[0] = 2.5f;
+		expect[1] = 3.5f;
+		expect[2] = 5.5f;
+		expect[3] = 6.5f;
+		expect[4] = 2f;
+		expect[5] = 8.5f;
+		expect[6] = 3.9f;
+		expect[7] = 5.1f;
+		expect[8] = 6.7f;
+		expect[9] = 20f;
+		expect[10] = 2.1f;
+		expect[11] = 15.5f;
+		expect[12] = 2.9f;
+		expect[13] = 7.3f;
+		expect[14] = 4f;
+		expect[15] = 11.5f;
+		expect[16] = 3.3f;
+		expect[17] = 8.5f;
+		expect[18] = 7.7f;
+		expect[19] = 6f;
+		expect[20] = 10.5f;
+		expect[21] = 23.5f;
+		expect[22] = 14.5f;
+		expect[23] = 26.5f;
+		expect[24] = 8f;
+		expect[25] = 15.2f;
+		expect[26] = 9.2f;
+		expect[27] = 5.4f;
+		expect[28] = 6.6f;
+		expect[29] = 2f;
 	}
 
 	@After
@@ -90,6 +125,60 @@ public class SelectorTest {
 		float cp[] = new float[5];
 		for(i = 0; i < 5; i++) {
 			ap[i] = al[i] / suma;
+			cp[i] = counter[i] / sumc;
+			assertEquals(ap[i], cp[i], delta);
+		}
+	}
+	
+	@Test
+	public void testSelect2() {
+		int i;
+		float suma = 0, sumc = 0;
+		int[] counter = new int[expectN];
+		for(i = 0; i < expectN; i++) counter[i] = 0;
+		int e = -1;
+		int n = repeatNum2;
+		for(i = 0; i < n; i++) {
+			e = Selector.select(expect);
+			assertTrue(e >= 0 && e < expectN);
+			counter[e]++;
+		}
+		for(i = 0; i < 5; i++) {
+			suma += expect[i];
+			sumc += counter[i];
+		}
+		float ap[] = new float[5];
+		float cp[] = new float[5];
+		for(i = 0; i < 5; i++) {
+			ap[i] = expect[i] / suma;
+			cp[i] = counter[i] / sumc;
+			assertEquals(ap[i], cp[i], delta);
+		}
+	}
+	
+	@Test
+	public void testAliasSelect2() {
+		int i;
+		float suma = 0, sumc = 0;
+		int[] counter = new int[expectN];
+		for(i = 0; i < expectN; i++) counter[i] = 0;
+		int e = -1;
+		int n = repeatNum2;
+		float probs[] = Selector.toProbs(expect);
+		ProbabilityPair[] jq = Selector.alias_setup(probs);
+		for(i = 0; i < n; i++) {
+			e = Selector.alias_draw(jq);
+			assertTrue(e >= 0 && e < expectN);
+			counter[e]++;
+		}
+		for(i = 0; i < 5; i++) {
+			suma += expect[i];
+			sumc += counter[i];
+		}
+		float ap[] = new float[5];
+		float cp[] = new float[5];
+		for(i = 0; i < 5; i++) {
+			ap[i] = expect[i] / suma;
 			cp[i] = counter[i] / sumc;
 			assertEquals(ap[i], cp[i], delta);
 		}
